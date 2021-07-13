bfdf973f9d93da1611bfd6b8bac6bc3f14aee4f3
Nicolò Fretti <nico.fretti@gmail.com>
2021-07-13 12:23:47 +0200

    Edit some import, fixing jumping

--- Scenes/KinematicBody2D.gd
+++ Scenes/KinematicBody2D.gd
@@ -1,31 +1,34 @@
 extends KinematicBody2D
 
-
-const gravity = 2
+const GRAVITY = 2
+const JUMP = -20
+const M = 12000
 func _physics_process(delta):
 	var direction = Vector2()
-	direction.x += (Input.get_action_strength("ui_right")*1 - Input.get_action_strength("ui_left")*1)
-	update_animation(direction)
+	direction.x += (Input.get_action_strength("ui_right")*1 -
+					Input.get_action_strength("ui_left")*1)
 	
 	if Input.is_action_just_pressed("ui_up") && is_on_floor():
-		direction.y = -35
-	else:
-		direction.y += gravity
-	move_and_slide(direction*200, Vector2(0,-1))
+		direction.y = JUMP
+	update_animation(direction)
+	direction.y += GRAVITY
+	move_and_slide(direction*delta*M, Vector2(0,-1))
 	
 	
 
 func update_animation(direction):
-	if(direction==Vector2()):
+	print(direction)
+	if direction.y == JUMP:
+		$Sprite.play("Jump")
+	elif (direction==Vector2()):
 		$Sprite.play("Idle")
-	elif direction.y==1:
-		$Sprite.play("Run")
-	elif direction.y==-1:
-		$Sprite.play("Run")
 	elif direction.x==-1:
 		$Sprite.play("Run")
 		$Sprite.flip_h=true
 	elif direction.x==1:
 		$Sprite.play("Run")
 		$Sprite.flip_h=false
+	
+	else:
+		$Sprite.play("Run")
 
